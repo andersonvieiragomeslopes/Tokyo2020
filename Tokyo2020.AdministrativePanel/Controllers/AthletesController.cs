1f06e4d93c5bd0fb5822ab62ce31cf32c5ded403
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-02-24 14:01:32 -0300

    Views do projeto github2

---
+++ Tokyo2020.AdministrativePanel/Controllers/AthletesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tokyo2020.AdministrativePanel.Models;
+using Tokyo2020.Domain.Models;
+
+namespace Tokyo2020.AdministrativePanel.Controllers
+{
+    public class AthletesController : Controller
+    {
+        private DataContextLocal db = new DataContextLocal();
+
+        // GET: Athletes
+        public ActionResult Index()
+        {
+            var athletes = db.Athletes.Include(a => a.Countries);
+            return View(athletes.ToList());
+        }
+
+        // GET: Athletes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // GET: Athletes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdCountry = new SelectList(db.Countries, "IdCountry", "Name");
+            return View();
+        }
+
+        // POST: Athletes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Athlete athlete)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Athletes.Add(athlete);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdCountry = new SelectList(db.Countries, "IdCountry", "Name", athlete.IdCountry);
+            return View(athlete);
+        }
+
+        // GET: Athletes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdCountry = new SelectList(db.Countries, "IdCountry", "Name", athlete.IdCountry);
+            return View(athlete);
+        }
+
+        // POST: Athletes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Athlete athlete)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(athlete).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdCountry = new SelectList(db.Countries, "IdCountry", "Name", athlete.IdCountry);
+            return View(athlete);
+        }
+
+        // GET: Athletes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Athlete athlete = db.Athletes.Find(id);
+            if (athlete == null)
+            {
+                return HttpNotFound();
+            }
+            return View(athlete);
+        }
+
+        // POST: Athletes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Athlete athlete = db.Athletes.Find(id);
+            db.Athletes.Remove(athlete);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Tokyo2020.AdministrativePanel/Controllers/CountriesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Tokyo2020.AdministrativePanel.Models;
+using Tokyo2020.Domain.Models;
+
+namespace Tokyo2020.AdministrativePanel.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private DataContextLocal db = new DataContextLocal();
+
+        // GET: Countries
+        public ActionResult Index()
+        {
+            return View(db.Countries.ToList());
+        }
+
+        // GET: Countries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Countries/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Countries.Add(country);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(country);
+        }
+
+        // GET: Countries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+
+        // POST: Countries/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(country).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(country);
+        }
+
+        // GET: Countries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Country country = db.Countries.Find(id);
+            if (country == null)
+            {
+                return HttpNotFound();
+            }
+            return View(country);
+        }
+
+        // POST: Countries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Country country = db.Countries.Find(id);
+            db.Countries.Remove(country);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
