211bf66c82697c62d407e6d57908c5c9d7e0f384
anderson vieira gomes <andersonvieira123456@gmail.com>
2020-02-24 13:57:23 -0300

    Problemas com o versionamento resolvidos

---
+++ Tokyo2020.AdministrativePanel/Models/DataContextLocal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Tokyo2020.Domain.Models;
+
+namespace Tokyo2020.AdministrativePanel.Models
+{
+    public class DataContextLocal : DataContext
+    {
+    }
+}
